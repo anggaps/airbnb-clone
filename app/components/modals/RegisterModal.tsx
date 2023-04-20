35349e32e548c411a4eb07bfa678a4fb1355206c
Angga ps <80311292+anggaps@users.noreply.github.com>
2023-04-20 21:05:20 +0700

    registermodals

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
+
+import useRegisterModals from '@/app/hooks/useRegisterModals';
+
+const RegisterModal = () => {
+  return <div></div>;
+};
+
+export default RegisterModal;
